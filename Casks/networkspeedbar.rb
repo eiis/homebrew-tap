4a854e852aa6caace808f8410d5a8780592d887e
@e_iism <88193046+eiis@users.noreply.github.com>
2026-03-28 20:47:07 +0800

    chore: update sha256 for v1.0.0

--- Casks/networkspeedbar.rb
+++ Casks/networkspeedbar.rb
@@ -1,6 +1,6 @@
 cask "networkspeedbar" do
   version "1.0.0"
-  sha256 :no_check
+  sha256 "d6c59c32502c04306d988b720cb5c4748a6953cc28faf5f152c8d859f04f3208"
 
   url "https://github.com/eiis/NetworkSpeedBar/releases/download/v#{version}/NetworkSpeedBar-#{version}.zip"
   name "NetworkSpeedBar"
